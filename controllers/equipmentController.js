975ff3b517d8569e7e3c32a2a15b3f8d927a3403
Khoi Nguyen <khoi.nguyen@ucalgary.ca>
2023-02-18 16:16:08 -0700

    asd

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -2,7 +2,12 @@ const db = require('../models');
 // const associate = require("../models/Associations/modelAssociations");
 const sequelize = require('sequelize');
 
+const users = db.users;
+
+const getUsers = async (req, res) => {
+    let response = await users.findAll({});
+};
 
 module.exports = {
-    
+    getUsers
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const equipmentController = require('../controllers/equipmentController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getUsers', function(req, res, next) {
+  equipmentController.getUsers;
 });
 
 module.exports = router;
